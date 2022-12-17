e1e6e5db850b02305cf191cdebecb41a1ec95ffd
Thiago Oliveira <59004790+thiagooliveiradev@users.noreply.github.com>
2022-12-17 12:50:14 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿
+using System.Threading;
+
+Menu();
+
+void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("S = Segundos => 10s = 10 segundos");
+    Console.WriteLine("M = Minutos => 1m = 1 minuto");
+    Console.WriteLine("0 = Sair");
+    Console.WriteLine("Quanto tempo deseja contar?");
+
+    string data = Console.ReadLine().ToLower();
+    char type = char.Parse(data.Substring(data.Length -1,1));
+    int time = int.Parse(data.Substring(0, data.Length -1));
+    int multiplier = 1;
+   
+    if (type =='m')
+        multiplier = 60;
+
+    if (time == 0)
+        System.Environment.Exit(0);
+
+    PreStart(time * multiplier);        
+   
+    Console.WriteLine(type);
+    Console.WriteLine(time);
+
+}
+
+void PreStart(int time)
+{
+    Console.Clear();
+    Console.WriteLine("Ready...");
+    Thread.Sleep(1000);
+    Console.WriteLine("Set...");
+    Thread.Sleep(1000);
+    Console.WriteLine("Go!");
+    Thread.Sleep(2500);
+
+    Start(time);
+}
+
+void Start(int time)
+{
+    int curretTime = 0;
+    while (curretTime != time)
+        {
+            Console.Clear();
+            curretTime++;
+            Console.WriteLine(curretTime);
+            Thread.Sleep(1000);
+        }
+
+    Console.Clear();
+    Console.WriteLine("StopWatch finalizado!");
+    Thread.Sleep(2500); 
+    Menu();   
+
+}
